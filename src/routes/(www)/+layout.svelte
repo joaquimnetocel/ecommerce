6cf379f2298901ede94af321f06ca4964ce49d26
joaquimnetocel <joaquim.neto.cel@gmail.com>
2026-07-01 09:27:12 -0300

    tudo 3

---
+++ src/routes/(www)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import BarraMobileInferior from '$lib/componentes/compartilhados/BarraMobileInferior/BarraMobileInferior.svelte';
+	import Rodape from '$lib/componentes/compartilhados/Rodape/Rodape.svelte';
+
+	let { children } = $props();
+</script>
+
+<div class="flex min-h-screen flex-col justify-between">
+	<main class="min-h-screen flex-1">
+		{@render children()}
+	</main>
+	<Rodape />
+	<BarraMobileInferior />
+</div>
---
+++ src/routes/(www)/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { PUBLIC_LOJA } from '$env/static/public';
+	import SliderDeTextosHorizontal from '$lib/componentes/compartilhados/SliderDeTextosHorizontal.svelte';
+	import SliderDeTextosVertical from '$lib/componentes/compartilhados/SliderDeTextosVertical.svelte';
+	import Banners from '$lib/componentes/especificosParaRotas/www/Banners.svelte';
+	import BarraDeNavegacao from '$lib/componentes/especificosParaRotas/www/BarraDeNavegacao/BarraDeNavegacao.svelte';
+	import { funcaoConfiguracoes } from '$lib/configuracoes/funcaoConfiguracoes';
+
+	const configuracoes = funcaoConfiguracoes({
+		loja: PUBLIC_LOJA,
+		quais: ['sliderDeTextosHorizontal', 'sliderDeTextosVertical', 'descricao', 'banners'],
+	});
+</script>
+
+<h1 class="sr-only">{configuracoes.descricao}</h1>
+
+<SliderDeTextosVertical
+	textos={configuracoes.sliderDeTextosVertical.textos}
+	duracao={configuracoes.sliderDeTextosVertical.duracao}
+/>
+
+<BarraDeNavegacao />
+
+<Banners />
+
+<SliderDeTextosHorizontal
+	textos={configuracoes.sliderDeTextosHorizontal.textos}
+	duracao={configuracoes.sliderDeTextosHorizontal.duracao}
+/>
